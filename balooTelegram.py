c088e7afbae18cef563b88c98a88a8a79f697ce1
Reyna Deantes <158773042+RDeantes@users.noreply.github.com>
2026-04-20 08:25:00 -0600

    Refactor responder function and clean imports

--- balooTelegram.py
+++ balooTelegram.py
@@ -1,30 +1,20 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-
 from BalooBrain import procesar
-
-# 🔐 Token con fallback
 
 TOKEN = os.getenv ("8726225258:AAGxNgGdE49I1-bz5u8-NYoU2amHHL20Ra4") or "8726225258:AAGxNgGdE49I1-bz5u8-NYoU2amHHL20Ra4"
 
-
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    mensaje_usuario = update.message.text
-    
-    respuesta = procesar(mensaje_usuario)
+    texto = update.message.text
+    respuesta = procesar(texto)
 
-    print("RESPUESTA:", respuesta)
-
-    # 📄 Si es archivo → lo manda
     if isinstance(respuesta, str) and os.path.isfile(respuesta):
         with open(respuesta, "rb") as f:
             await update.message.reply_document(document=f)
     else:
         await update.message.reply_text(respuesta)
 
-
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, responder))
-
 app.run_polling()
