f4e9af337e6605b201eade81e58159f008c6713c
Reyna Deantes <158773042+RDeantes@users.noreply.github.com>
2026-04-19 20:03:52 -0600

    Refactor token handling and add debug output
    Updated the token retrieval to use environment variables and added a debug print statement.

--- balooTelegram.py
+++ balooTelegram.py
@@ -1,11 +1,10 @@
-
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 from BalooBrain import procesar
 
-TOKEN = "8726225258:AAGxNgGdE49I1-bz5u8-NYoU2amHHL20Ra4" # 🔐 importante
+TOKEN = os.getenv("8726225258:AAGxNgGdE49I1-bz5u8-NYoU2amHHL20Ra4")
 
 
 def ejecutar_agente(texto):
@@ -17,7 +16,9 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     respuesta = ejecutar_agente(mensaje_usuario)
 
-    # 🔥 TODO esto VA DENTRO de la función
+    print("RESPUESTA:", respuesta)  # 👈 debug
+
+    # 🔥 AQUÍ PASA LA MAGIA
     if os.path.exists(respuesta):
         with open(respuesta, "rb") as f:
             await update.message.reply_document(document=f)
